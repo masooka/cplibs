e47d780366801e6787a4f1814bcd3b91248c89fb
Masooka <masooka@live.com>
2023-09-08 11:08:44 -0700

    Use doubly-linked list in find_eulerian_circuit
    This is faster than HashMap but consumes more memory especially when the
    adjacency matrix is sparse.

--- crates/graph/eulerian/src/lib.rs
+++ crates/graph/eulerian/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 /// Finds an Eulerian circuit in an undirected graph represented by an adjacency
 /// matrix.
 ///
@@ -15,6 +13,13 @@ use std::collections::HashMap;
 /// assert!(find_eulerian_circuit(&mut adj_matrix).is_some());
 /// ```
 pub fn find_eulerian_circuit(adj_matrix: &[Vec<u32>]) -> Option<Vec<usize>> {
+    #[derive(Clone, Copy, Default)]
+    struct Node {
+        value: u32,
+        prev: Option<usize>,
+        next: Option<usize>,
+    }
+
     // Check if all vertices have even degree
     if !adj_matrix
         .iter()
@@ -25,12 +30,21 @@ pub fn find_eulerian_circuit(adj_matrix: &[Vec<u32>]) -> Option<Vec<usize>> {
 
     let n = adj_matrix.len();
 
-    // Create a set for each vertex to store its unvisited neighbors
-    let mut neighbors: Vec<HashMap<usize, u32>> = vec![HashMap::new(); n];
+    // Create a list for each vertex to store its unvisited neighbors
+    let mut neighbors = vec![vec![Node::default(); n]; n];
+    let mut heads = vec![None; n];
     for i in 0..n {
+        let mut prev: Option<usize> = None;
         for j in 0..n {
             if adj_matrix[i][j] > 0 {
-                neighbors[i].insert(j, adj_matrix[i][j]);
+                neighbors[i][j].value = adj_matrix[i][j];
+                if let Some(prev) = prev {
+                    neighbors[i][prev].next = Some(j);
+                    neighbors[i][j].prev = Some(prev);
+                } else {
+                    heads[i] = Some(j);
+                }
+                prev = Some(j);
             }
         }
     }
@@ -42,14 +56,27 @@ pub fn find_eulerian_circuit(adj_matrix: &[Vec<u32>]) -> Option<Vec<usize>> {
         let v = *stack.last().unwrap();
 
         // Find an adjacent vertex with an edge to traverse
-        if let Some((&u, &count)) = neighbors[v].iter().next() {
-            // Remove the edge between u and v
-            if count > 1 {
-                neighbors[u].insert(v, count - 1);
-                neighbors[v].insert(u, count - 1);
-            } else {
-                neighbors[u].remove(&v);
-                neighbors[v].remove(&u);
+        if let Some(u) = heads[v] {
+            neighbors[v][u].value -= 1;
+            neighbors[u][v].value -= 1;
+            if neighbors[v][u].value == 0 {
+                if let Some(prev) = neighbors[v][u].prev {
+                    neighbors[v][prev].next = neighbors[v][u].next;
+                } else {
+                    heads[v] = neighbors[v][u].next;
+                }
+                if let Some(next) = neighbors[v][u].next {
+                    neighbors[v][next].prev = neighbors[v][u].prev;
+                }
+
+                if let Some(prev) = neighbors[u][v].prev {
+                    neighbors[u][prev].next = neighbors[u][v].next;
+                } else {
+                    heads[u] = neighbors[u][v].next;
+                }
+                if let Some(next) = neighbors[u][v].next {
+                    neighbors[u][next].prev = neighbors[u][v].prev;
+                }
             }
             stack.push(u);
         } else {
@@ -60,7 +87,7 @@ pub fn find_eulerian_circuit(adj_matrix: &[Vec<u32>]) -> Option<Vec<usize>> {
     }
 
     // Check if we've traversed all edges
-    if neighbors.iter().all(|map| map.is_empty()) {
+    if heads.iter().all(|head| head.is_none()) {
         Some(cycle)
     } else {
         None
