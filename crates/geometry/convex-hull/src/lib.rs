3eddc79c6a13382dae84cbab96d6b5275a258769
Masooka <masooka@live.com>
2023-09-13 23:44:28 +0000

    Add convex_hull_counterclockwise

--- crates/geometry/convex-hull/src/lib.rs
+++ crates/geometry/convex-hull/src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(clippy::type_complexity)]
 
-use std::ops::{Mul, Sub};
+use std::{
+    mem,
+    ops::{Mul, Sub},
+};
 
 /// Finds the convex hull of a set of points.
 ///
@@ -43,6 +46,90 @@ where
     hull.pop();
 
     hull
+}
+
+/// Finds the convex hull of a set of points. It returns the points in sorted
+/// order along with the indices of the points that make up the hull.
+pub fn convex_hull_counterclockwise<C>(
+    mut points: Vec<(C, C)>,
+    hull_include_midpoints: bool,
+) -> (Vec<(C, C)>, Vec<usize>)
+where
+    C: Copy + Default + PartialOrd + Sub<Output = C> + Mul<Output = C>,
+{
+    points.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    let turn_direction = if hull_include_midpoints {
+        counterclockwise_or_collinear
+    } else {
+        counterclockwise
+    };
+    let hull = convex_hull_sorted(&points, turn_direction);
+    (points, hull)
+}
+
+fn convex_hull_sorted<C: PartialOrd>(
+    sorted_points: &[(C, C)],
+    turn_direction: fn(&(C, C), &(C, C), &(C, C)) -> bool,
+) -> Vec<usize> {
+    let mut hull = Vec::new();
+
+    let mut half: Vec<usize> = Vec::new();
+    convex_hull_sorted_half(
+        sorted_points,
+        0..sorted_points.len(),
+        &mut half,
+        turn_direction,
+    );
+
+    half.pop();
+    mem::swap(&mut hull, &mut half);
+    convex_hull_sorted_half(
+        sorted_points,
+        (0..sorted_points.len()).rev(),
+        &mut half,
+        turn_direction,
+    );
+    half.pop();
+    hull.extend(half);
+
+    hull
+}
+
+pub fn convex_hull_sorted_half_counterclockwise<C>(
+    sorted_points: &[(C, C)],
+    iter: impl Iterator<Item = usize>,
+    hull_include_midpoints: bool,
+) -> Vec<usize>
+where
+    C: Copy + Default + PartialOrd + Sub<Output = C> + Mul<Output = C>,
+{
+    let turn_direction = if hull_include_midpoints {
+        counterclockwise_or_collinear
+    } else {
+        counterclockwise
+    };
+    let mut half = Vec::new();
+    convex_hull_sorted_half(sorted_points, iter, &mut half, turn_direction);
+    half
+}
+
+fn convex_hull_sorted_half<C>(
+    points: &[(C, C)],
+    iter: impl Iterator<Item = usize>,
+    half: &mut Vec<usize>,
+    turn_direction: fn(&(C, C), &(C, C), &(C, C)) -> bool,
+) {
+    for i in iter {
+        while half.len() >= 2 {
+            let o = &points[*half.get(half.len() - 2).unwrap()];
+            let a = &points[*half.get(half.len() - 1).unwrap()];
+            if turn_direction(o, a, &points[i]) {
+                break;
+            }
+            half.pop();
+        }
+        half.push(i);
+    }
 }
 
 /// Determines if three points make a clockwise turn.
@@ -142,5 +229,54 @@ mod tests {
 
         let hull = super::convex_hull(points, |&(x, y)| (x, y), super::counterclockwise);
         assert_eq!(hull, &[(0, 0), (2, -2), (4, 0), (2, 2)]);
+    }
+
+    #[test]
+    fn partition() {
+        let points = vec![
+            (0, 0),
+            (1, 1),
+            (2, 2),
+            (3, 1),
+            (4, 0),
+            (3, -1),
+            (2, -2),
+            (1, -1),
+        ];
+
+        let (_, hull) = super::convex_hull_counterclockwise(points, false);
+        assert_eq!(hull, &[0, 3, 7, 4]);
+    }
+
+    #[test]
+    fn partition_last_pop() {
+        let points = vec![
+            (-5, 4),
+            (-4, 2),
+            (-3, 2),
+            (-2, -5),
+            (-1, 3),
+            (2, -5),
+            (2, -2),
+            (2, -1),
+        ];
+        let (_, hull) = super::convex_hull_counterclockwise(points, false);
+        assert_eq!(hull, &[0, 3, 5, 7, 4]);
+    }
+
+    #[test]
+    fn partition_middle_pop() {
+        let points = vec![
+            (-4, -5),
+            (-4, -3),
+            (-3, 4),
+            (1, -5),
+            (3, -2),
+            (3, -1),
+            (3, 4),
+            (4, -5),
+        ];
+        let (_sorted, hull) = super::convex_hull_counterclockwise(points, false);
+        assert_eq!(hull, &[0, 7, 6, 2, 1]);
     }
 }
