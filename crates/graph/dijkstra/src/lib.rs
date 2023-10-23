a77fe3665e2fa9db1f4a86bcca91cef0ad40af2d
Masooka <masooka@live.com>
2023-10-23 22:21:09 +0000

    Add dijkstra::costs

--- crates/graph/dijkstra/src/lib.rs
+++ crates/graph/dijkstra/src/lib.rs
@@ -5,6 +5,31 @@ use std::{
     ops::{Add, AddAssign},
 };
 
+pub fn costs<V, Es, Vs, Ws, WsI, W>(start: V, mut neighbors: Es, mut costs: Ws, mut cost: WsI) -> Ws
+where
+    V: Copy + Ord,
+    Es: FnMut(V) -> Vs,
+    Vs: IntoIterator<Item = (V, W)>,
+    WsI: FnMut(&mut Ws, V) -> &mut W,
+    W: Copy + Ord + Add<Output = W> + Sum,
+{
+    *cost(&mut costs, start) = iter::empty().sum();
+    let queue = &mut BinaryHeap::from(vec![(Reverse(iter::empty().sum()), start)]);
+    while let Some((Reverse(current_cost), current_node)) = queue.pop() {
+        if *cost(&mut costs, current_node) < current_cost {
+            continue;
+        }
+        for (next_node, cost_delta) in neighbors(current_node) {
+            let next_cost = current_cost + cost_delta;
+            if next_cost < *cost(&mut costs, next_node) {
+                *cost(&mut costs, next_node) = next_cost;
+                queue.push((Reverse(next_cost), next_node));
+            }
+        }
+    }
+    costs
+}
+
 pub fn costs_and_counts<V, Es, Vs, Ws, WsI, W, Cs, CsI, C>(
     start: V,
     mut neighbors: Es,
