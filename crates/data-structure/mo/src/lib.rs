f6ccbba806b7f8d0685d872c7324d963f36796ea
Masooka <masooka@live.com>
2023-10-30 18:48:22 +0000

    Use Hilbert order in Mo's

--- crates/data-structure/mo/src/lib.rs
+++ crates/data-structure/mo/src/lib.rs
@@ -8,32 +8,62 @@ pub trait Context {
     fn shrink_r(&mut self);
 }
 
-pub struct Query {
+struct Query {
     l: usize,
     r: usize,
     idx: usize,
+    ord: usize,
 }
 
-pub fn apply<C: Context, const B: usize>(queries: &[(usize, usize)], ctx: &mut C) -> Vec<usize> {
+#[inline]
+fn hilbert_order(x: usize, y: usize, pow: usize, rotate: usize) -> usize {
+    if pow == 0 {
+        return 0;
+    }
+    let hpow = 1 << (pow - 1);
+    let mut seg = if x < hpow {
+        if y < hpow {
+            0
+        } else {
+            3
+        }
+    } else if y < hpow {
+        1
+    } else {
+        2
+    };
+    seg = (seg + rotate) & 3;
+    let rotate_delta = [3, 0, 0, 1];
+    let nx = x & (x ^ hpow);
+    let ny = y & (y ^ hpow);
+    let nrot = (rotate + rotate_delta[seg]) & 3;
+    let sub_square_size = 1 << (2 * pow - 2);
+    let ans = seg * sub_square_size;
+    let add = hilbert_order(nx, ny, pow - 1, nrot);
+    ans + if seg == 1 || seg == 2 {
+        add
+    } else {
+        sub_square_size - add - 1
+    }
+}
+
+/// Applies Mo's algorithm to the given queries. `B` is the block size, and `L`
+/// is the log of the maximum value of the queries.
+pub fn apply<C: Context, const B: usize, const L: usize>(
+    queries: &[(usize, usize)],
+    ctx: &mut C,
+) -> Vec<usize> {
     let mut queries = queries
         .iter()
         .enumerate()
-        .map(|(idx, &(l, r))| Query { l, r, idx })
+        .map(|(idx, &(l, r))| Query {
+            l,
+            r,
+            idx,
+            ord: hilbert_order(l, r, L, 0),
+        })
         .collect::<Vec<_>>();
-    queries.sort_unstable_by(|a, b| {
-        let block_a = a.l / B;
-        let block_b = b.l / B;
-        match block_a.cmp(&block_b) {
-            std::cmp::Ordering::Equal => {
-                if block_a % 2 == 0 {
-                    b.r.cmp(&a.r)
-                } else {
-                    a.r.cmp(&b.r)
-                }
-            }
-            other => other,
-        }
-    });
+    queries.sort_unstable_by_key(|q| q.ord);
     let mut ans = vec![0; queries.len()];
     for q in queries {
         while ctx.l() > q.l {
